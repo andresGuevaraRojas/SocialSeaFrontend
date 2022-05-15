99e3a0bc942bb55370c7b716fd42ba6e7df36068
Andres Guevara <=>
2022-05-15 16:13:46 -0500

    Se le agrega id a los posts

--- src/data/Posts.js
+++ src/data/Posts.js
@@ -1,5 +1,6 @@
 const posts = [
     {
+        id:1,
         userId:12,
         category:'claps',
         image:'https://cdn.pixabay.com/photo/2019/11/07/08/12/sea-4608198_960_720.jpg',
@@ -7,6 +8,7 @@ const posts = [
         description:'Playa muy limpia'
     },
     {
+        id:2,
         userId:13,
         category:'claps',
         image:'https://www.worldbank.org/content/dam/photos/780x439/2022/jan-2/deep-blue-sea_shutterstock_1512728810.jpg',
@@ -14,6 +16,7 @@ const posts = [
         description:'La arena estaba limpia'
     },
     {
+        id:3,
         userId:12,
         category:'flutters',
         image:'https://thumbs.dreamstime.com/b/dirty-beaches-caused-dumping-undisciplined-70294453.jpg',
@@ -21,6 +24,7 @@ const posts = [
         description:'Residuos plasticos'
     },
     {
+        id:4,
         userId:12,
         category:'dolphins',
         image:'https://oceanfdn.org/wp-content/uploads/2010/08/SargassoSea-1-1200x600.jpg',
