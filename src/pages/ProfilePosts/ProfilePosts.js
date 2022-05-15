4ecf6195b5569b8a00763f4fbb9ecc43fcae19df
Andres Guevara <=>
2022-05-15 10:56:46 -0500

    Se actualizan estilos de ProfilePosts

--- src/pages/ProfilePosts/ProfilePosts.js
+++ src/pages/ProfilePosts/ProfilePosts.js
@@ -1,9 +1,24 @@
-function ProfilePosts(){
-    console.log('hola')
-    return(
-        <section>
-            hola
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import PostCategories from "../../shared/PostCategories"
+import style from './ProfilePosts.module.css'
+function ProfilePosts() {
+    let params = useParams()
+    let category = params.category || 'claps'
+
+    const [posts, setPosts] = useState([])
+
+    return (
+        <section className={style.posts}>
+            <h1>
+                {
+                    PostCategories[category]
+                }
+            </h1>
+            <div className={style.container}>
+                
+            </div>
         </section>
-    )    
+    )
 }
 export default ProfilePosts
