5f9720023aa98126489b00cf132438f30b51bf82
Andres Guevara <=>
2022-05-15 02:24:24 -0500

    Se modifican estilos de la pagina profiles

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,9 +1,16 @@
 import AboutProfile from "../../components/AboutProfile/AboutProfile"
+import style from "./Profile.module.css"
 
 function Profile() {
     return(
-        <main>
-            <AboutProfile name={'Nanci Carrillo'} image={'https://images.pexels.com/photos/10896817/pexels-photo-10896817.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'} about={'👑'}/>
+        <main className={style.main}>
+            <section className={style.about}>
+                <AboutProfile 
+                    name={'Nanci Carrillo'} 
+                    image={'https://images.pexels.com/photos/10896817/pexels-photo-10896817.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'} 
+                    about={'👑'}
+                />
+            </section>            
         </main>
     )
 }
