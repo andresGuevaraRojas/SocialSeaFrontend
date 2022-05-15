b848ddfca128c6d35ef265f2dcb96de566ffe5fd
Andres Guevara <=>
2022-05-15 11:14:12 -0500

    Se actualiza la informacion de Posts.js para incluir el id del usuario

--- src/data/Posts.js
+++ src/data/Posts.js
@@ -1,23 +1,27 @@
 const posts = [
     {
+        userId:12,
         category:'claps',
         image:'https://cdn.pixabay.com/photo/2019/11/07/08/12/sea-4608198_960_720.jpg',
         location:'Playa del carmen',
         description:'Playa muy limpia'
     },
     {
+        userId:13,
         category:'claps',
         image:'https://www.worldbank.org/content/dam/photos/780x439/2022/jan-2/deep-blue-sea_shutterstock_1512728810.jpg',
         location:'Playa del malecon',
         description:'La arena estaba limpia'
     },
     {
+        userId:12,
         category:'flutters',
         image:'https://thumbs.dreamstime.com/b/dirty-beaches-caused-dumping-undisciplined-70294453.jpg',
         location:'Playa X',
         description:'Residuos plasticos'
     },
     {
+        userId:12,
         category:'dolphins',
         image:'https://oceanfdn.org/wp-content/uploads/2010/08/SargassoSea-1-1200x600.jpg',
         location:'Playa del pacifico',
